2441c038b1f251829414ba8a64bb49d9dcff328b
shailendra <Sam724673@gmail.com>
2026-01-11 21:18:29 +0530

    Initial Dart project setup

---
+++ bin/text_calculator_.dart
@@ -0,0 +1,5 @@
+import 'package:text_calculator_/text_calculator_.dart' as text_calculator_;
+
+void main(List<String> arguments) {
+  print('Hello world: ${text_calculator_.calculate()}!');
+}
---
+++ test/text_calculator__test.dart
@@ -0,0 +1,8 @@
+import 'package:text_calculator_/text_calculator_.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
